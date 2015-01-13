c7f09decf2848c66c35fe396112863cc2470b324
Jim McAndrew <jim@loc8.us>
2015-01-13 09:29:45 -0700

    Remove the updateIdSettings file and move it to iD

--- updateIdSettings.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-var idSettings = require('./config').iD;
-var settingsFilePath = process.argv[2];
-var settingsFile = settingsFilePath + '/js/id/npmap.js';
-
-console.log('Copying file:' + settingsFile);
-fs.writeFileSync(settingsFile,  'iD.npmap = ' + JSON.stringify(idSettings, null, 4));
