abd9ce5922b4b534acc71e5b8bf65afb4e6b5f26
Jim McAndrew <jim@loc8.us>
2015-07-30 09:48:25 -0600

    Update formatting and express-handlebars plugin: Update service.js

--- service.js
+++ service.js
@@ -5,19 +5,18 @@
 // npm link node-windows
 // node service.js
 
-
 var Service = require('node-windows').Service;
 
 // Create a new service object
 var svc = new Service({
-  name:'Places Website Node.js App',
+  name: 'Places Website Node.js App',
   description: 'the places-website for the node.js app that runs the Places project.',
   script: 'C:\\places-website\\index.js'
 });
 
 // Listen for the "install" event, which indicates the
 // process is available as a service.
-svc.on('install',function(){
+svc.on('install', function () {
   svc.start();
 });
 
