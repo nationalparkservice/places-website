4c97708a54ab3557a8268a90b8bec93c0f576aa4
Jim McAndrew <jim@loc8.us>
2015-08-19 12:49:27 -0600

    Remove test code

--- readConfig.js
+++ readConfig.js
@@ -3,11 +3,9 @@ var fs = require('fs');
 var replace = {
   env: process.env,
   secrets: function (input) {
-    console.log('s__'+input);
     return JSON.parse(fs.readFileSync(__dirname + '/secrets/' + input[0] + '.json', 'utf8'));
   },
   environment: function (input) {
-    console.log('i__'+input);
     return JSON.parse(fs.readFileSync(__dirname + '/environments.json', 'utf8'))[process.env.NODE_ENV || 'development'][input[0]];
   }
 };
@@ -17,5 +15,4 @@ module.exports = function (config) {
   return fandlebars.obj(config, replace);
 };
 
-console.log(process.env.NODE_ENV || "dev");
-console.log(JSON.stringify(fandlebars.obj(require('./config'), replace)/*, null, 2*/));
+// console.log(JSON.stringify(fandlebars.obj(require('./config'), replace), null, 2));
